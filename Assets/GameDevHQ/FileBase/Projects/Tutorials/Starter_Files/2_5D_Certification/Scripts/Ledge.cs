28a0b9e5c0a03a5529aa94ac00840ad72216c52c
Eric Felts <echristianfelts@gmail.com>
2020-09-18 13:22:56 -0700

    initial commit.

---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Certification/Scripts/Ledge.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ledge : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 _handPos, _standPos;
+    [SerializeField]
+    private GameObject _handPosTarget;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _handPos = _handPosTarget.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ledge_Grab_Sensor"))
+        {
+            //try and get player component
+            var player = other.transform.parent.GetComponent<Player>();
+            if (player != null)
+            {
+                // call ledge grab method
+                player.GrabLedge(_handPos,this);
+            }
+        }
+    }
+
+    public Vector3 GetStandPos()
+    {
+        return _standPos;
+    }
+}
---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Certification/Scripts/Player.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //Speed
+    [SerializeField]
+    private float _speed = 5.0f;
+    //Jumpheight
+    [SerializeField]
+    private float _jumpForce = 5.0f;
+    //Gravity
+    [SerializeField]
+    private float _gravity = 1.0f;
+    //Direction
+    private Vector3 _direction;
+
+    private CharacterController _controller;
+    private Animator _anim;
+    private Collider _grabSensor;
+    private bool _jumping = false;
+    private bool _onLedge;
+    private Ledge _activeLedge;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+        _anim = GetComponentInChildren<Animator>();
+        _grabSensor = GetComponentInChildren<BoxCollider>();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        CalculateMovement();
+
+        if (_onLedge==true)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                _anim.SetTrigger("LedgeGrabTrigger");
+            }
+        }
+
+    }
+
+    void CalculateMovement()
+    {
+        // if grounded
+        if (_controller.isGrounded == true)
+        {
+            float h = Input.GetAxisRaw("Horizontal");
+            _direction = new Vector3(0, 0, h) * _speed;
+            _anim.SetFloat("Speed", Mathf.Abs(h));
+
+            // what dir are we facing..?
+            // if dir on x > o then face right. 
+            // else face left
+            if (h != 0)
+            {
+                Vector3 facing = transform.localEulerAngles;
+                facing.y = _direction.z > 0 ? 0 : 180;
+                transform.localEulerAngles = facing;
+            }
+
+
+            _anim.ResetTrigger("Jump");
+            if (_jumping)
+            {
+                _jumping = false;
+                _anim.SetBool("Jumping", _jumping);
+            }
+
+            // on space jump...
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _direction.y += _jumpForce;
+                _anim.SetTrigger("Jump");
+
+                _jumping = true;
+                _anim.SetBool("Jumping", _jumping);
+            }
+
+        }
+
+        _direction.y -= _gravity * Time.deltaTime;
+        //  calculate movement direction based on user inputs
+        // if jump 
+        //  adjust jump height
+        //  move
+        _controller.Move(_direction * Time.deltaTime);
+    }
+
+    public void GrabLedge(Vector3 handpos, Ledge currentLedge)
+    {
+        _anim.SetBool("LedgeGrabBool", true);
+        _jumping = false;
+        _anim.SetBool("Jumping", _jumping);
+
+        _controller.enabled = false;
+
+        //_anim.SetFloat("Speed", 0f);
+
+        _onLedge = true;
+        // update hand pos.
+        handpos.y -= 2.338f*3f;  // Player grab sensor offset height times scale.
+        transform.position = handpos;
+        _activeLedge = currentLedge;
+    }
+
+    public void ClimbUpComplete()
+    {
+        //  snap position.
+        transform.position = _activeLedge.GetStandPos();
+        //_anim.SetFloat("Speed", 0f);
+        _anim.SetBool("LedgeGrabBool", false);
+
+        _controller.enabled = true;
+
+    }
+}
